9787404026f6fe85ad2753c6d2252a30f6406b1d
lapin-anton <lapin_antoine@mail.ru>
2021-02-01 12:22:00 +0300

    Person checker test added

---
+++ city-register/src/test/java/ru/java_project/ext/dao/PersonCheckDaoTest.java
@@ -0,0 +1,47 @@
+package ru.java_project.ext.dao;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.java_project.ext.domain.PersonRequest;
+import ru.java_project.ext.domain.PersonResponse;
+import ru.java_project.ext.exception.PersonCheckException;
+
+import java.time.LocalDate;
+
+public class PersonCheckDaoTest extends TestCase {
+
+    @Test
+    public void testCheckPerson() throws PersonCheckException {
+        PersonRequest request = new PersonRequest();
+        request.setSurName("Иванов");
+        request.setGivenName("Иван");
+        request.setPatronymic("Иванович");
+        request.setDateOfBirth(LocalDate.of(1993, 2, 28));
+        request.setDistrictCode(1);
+        request.setStreetCode(1);
+        request.setBuilding("10");
+        request.setExtension("2");
+        request.setApartment("121");
+        PersonCheckDao dao = new PersonCheckDao();
+        PersonResponse response = dao.checkPerson(request);
+        Assert.assertTrue(response.isRegistered());
+        Assert.assertFalse(response.isTemporal());
+    }
+
+    @Test
+    public void testCheckPerson2() throws PersonCheckException {
+        PersonRequest request = new PersonRequest();
+        request.setSurName("Иванова");
+        request.setGivenName("Анна");
+        request.setPatronymic("Ивановна");
+        request.setDateOfBirth(LocalDate.of(1995, 8, 12));
+        request.setDistrictCode(1);
+        request.setStreetCode(1);
+        request.setBuilding("10");
+        PersonCheckDao dao = new PersonCheckDao();
+        PersonResponse response = dao.checkPerson(request);
+        Assert.assertFalse(response.isRegistered());
+        Assert.assertFalse(response.isTemporal());
+    }
+}
