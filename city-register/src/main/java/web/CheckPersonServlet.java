8e9d9286f77274ea8d1ccd2eb092c71fda8f6fda
lapin-anton <lapin_antoine@mail.ru>
2021-02-03 10:36:53 +0300

    added all checkied parametres

--- city-register/src/main/java/web/CheckPersonServlet.java
+++ city-register/src/main/java/web/CheckPersonServlet.java
@@ -12,8 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.DateFormatter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
@@ -33,17 +35,17 @@ public class CheckPersonServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
 
-        String surname = req.getParameter("surname");
         PersonRequest request = new PersonRequest();
-        request.setSurName(surname);
-        request.setGivenName("Иван");
-        request.setPatronymic("Иванович");
-        request.setDateOfBirth(LocalDate.of(1993, 2, 28));
-        request.setDistrictCode(1);
-        request.setStreetCode(1);
-        request.setBuilding("10");
-        request.setExtension("2");
-        request.setApartment("121");
+        request.setSurName(req.getParameter("surname"));
+        request.setGivenName(req.getParameter("givenname"));
+        request.setPatronymic(req.getParameter("patronymic"));
+        LocalDate date = LocalDate.parse(req.getParameter("dateOfBirth"), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        request.setDateOfBirth(date);
+        request.setDistrictCode(Integer.parseInt(req.getParameter("districtCode")));
+        request.setStreetCode(Integer.parseInt(req.getParameter("streetCode")));
+        request.setBuilding(req.getParameter("building"));
+        request.setExtension(req.getParameter("extension"));
+        request.setApartment(req.getParameter("apartment"));
         try {
             PersonResponse response = dao.checkPerson(request);
             if(response.isRegistered()) {
